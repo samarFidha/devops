bd8a641f8161861776b8bbb1c770a7b3645472cd
AmenAllahBelhouichet <amenallah.belhouichet@esprit.tn>
2025-03-01 21:28:44 +0100

    test2

--- src/main/java/tn/esprit/spring/dao/entities/Universite.java
+++ src/main/java/tn/esprit/spring/dao/entities/Universite.java
@@ -20,7 +20,8 @@ public class Universite implements Serializable {
     long idUniversite;
     String nomUniversite;
     String adresse;
-    @OneToOne(cascade = CascadeType.ALL) //ajout, Modif et supprim
+    @OneToOne(cascade = CascadeType.ALL)
+    //ajout, Modif et supprim
     Foyer foyer;
 
 
