862600a8aa43e0bbffd45cbe777ce3313f5ba8c2
bechirgrali <grali.bechir@esprit.tn>
2025-04-16 16:11:26 +0100

    grafana

--- Dockerfile
+++ Dockerfile
@@ -8,7 +8,7 @@ WORKDIR /app
 COPY target/Foyer-0.0.1-SNAPSHOT.jar app.jar
 
 # Étape 4 : Exposer le port (exemple : 8080)
-EXPOSE 8080
+EXPOSE 8081
 
 CMD ["java", "-jar", "app.jar"]
 
---
+++ src/main/java/tn/esprit/spring/service/Config/MetricsConfig.java
@@ -0,0 +1,16 @@
+package tn.esprit.spring.service.Config;
+
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MetricsConfig {
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+        return registry -> registry.config()
+                .commonTags("application", "foyer");
+    }
+}
